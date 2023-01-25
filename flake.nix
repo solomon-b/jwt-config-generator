380a75dac7a87b4a12158dc670c19609f536b308
solomon <ssbothwell@gmail.com>
2023-01-25 15:54:02 -0800

    update nix infra

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
 {
   description = "jwt-config";
 
-  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-21.05;
+  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-22.11;
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
-      defaultPackage.x86_64-linux = (pkgs.callPackage ./default.nix { }).package;
+      defaultPackage.x86_64-linux = pkgs.buildNpmPackage {
+        name = "jwt-gen";
+        src = ./.;
+        npmDepsHash = "sha256-da7XOML3fcX7oYAUWYLYDCwfM2s3dbojhtLuPqg+sZ0=";
+        dontNpmBuild = true;
+      };
 
       overlay = final: prev: {
         jwt-config = (final.callPackage ./default.nix { }).package ;
